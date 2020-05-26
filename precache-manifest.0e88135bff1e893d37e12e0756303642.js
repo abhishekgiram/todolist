26909caefad660140569dbbcce2d782e114739c4
abhishekgiram <abhishek.giram11@gmail.com>
2020-05-26 13:46:38 +0530

    Updates

---
+++ precache-manifest.0e88135bff1e893d37e12e0756303642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e6db829dd7cc5c21612121b94969fb",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e601b2dd3ad5ca0f4c08",
+    "url": "/todolist/static/css/main.75a8d625.chunk.css"
+  },
+  {
+    "revision": "8522b65662d4c3137729",
+    "url": "/todolist/static/js/2.360634be.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/todolist/static/js/2.360634be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e601b2dd3ad5ca0f4c08",
+    "url": "/todolist/static/js/main.b95c9aa6.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  }
+]);
